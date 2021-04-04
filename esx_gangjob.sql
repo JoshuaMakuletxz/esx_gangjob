291808f73a412b03a1063eb80276b440a85ad468
Joshuaaaa <69721993+Joshua-3@users.noreply.github.com>
2021-04-04 14:03:43 +0800

    Update esx_gangjob.sql

--- esx_gangjob.sql
+++ esx_gangjob.sql
@@ -1,25 +1,25 @@
 USE `fivem`;
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
-	('society_gang', 'Cartel De Luna', 1)
+	('society_gang', 'Gang', 1)
 ;
 
 INSERT INTO `datastore` (name, label, shared) VALUES
-	('society_gang', 'Cartel De Luna', 1)
+	('society_gang', 'Gang', 1)
 ;
 
 INSERT INTO `addon_inventory` (name, label, shared) VALUES
-	('society_gang', 'Cartel De Luna', 1)
+	('society_gang', 'Gang', 1)
 ;
 
 INSERT INTO `jobs` (name, label) VALUES
-	('gang', 'Cartel De Luna')
+	('gang', 'Gang')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-	('gang',0,'recruit','YOUNGENS',20,'{}','{}'),
-	('gang',1,'officer','HITMEN',40,'{}','{}'),
-	('gang',2,'sergeant','HIGH COUNCIL',60,'{}','{}'),
-	('gang',3,'lieutenant','GODFATHER/GODMOTHER',85,'{}','{}'),
-	('gang',4,'boss','FAMILIA FATHER',100,'{}','{}')
+	('gang',0,'recruit','Recruit',20,'{}','{}'),
+	('gang',1,'officer','Member',40,'{}','{}'),
+	('gang',2,'sergeant','Hitman',60,'{}','{}'),
+	('gang',3,'lieutenant','Assistant Boss',85,'{}','{}'),
+	('gang',4,'boss','Boss',100,'{}','{}')
 ;
